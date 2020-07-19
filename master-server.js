0a10d361ceebff21ad587dabb2a82c581dc0a474
Bernard <gicehajunior76@gmail.com>
2020-07-20 00:56:54 +0300

    New Functionality

--- master-server.js
+++ master-server.js
@@ -3,20 +3,34 @@ process.stdout.write('\u001B[2J\u001B[0;0f');
 
 // require the net module and create a server
 const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
 
 // connection handler @ an every time a client server is connected
 server.on('connection', socket => {
+    socket.id = counter++;
+    sockets[socket.id] = socket;
+
     console.log('Client is connected');
-    socket.write('Welcome new Client!\n');
+    socket.write('Please write your name below!');
 
     socket.on('data', data => {
-        console.log('Data Sent is:', data);
-        socket.write('data is:');
-        socket.write(data);
+        if (!sockets[socket.id]){
+            socket.name = data.toString.trim();
+            socket.write(`Welcome ${socket.name}\n`);
+            sockets[socket.id] = socket;
+            return;
+        }
+        Object.entries(sockets).forEach(([key, client_socket]) => {
+            if (socket.id == key ) return;
+            client_socket.write(`${socket.id}:`);
+            client_socket.write(data);
+        });
     });
 
     // emit a message to the master server for any client disconnection 
     socket.on('end', () => {
+        delete sockets[socket.id]
         console.log("The Client is disconnected");
     });
     
